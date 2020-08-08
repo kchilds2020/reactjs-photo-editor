40d20b1ed15c7d90d960a9931bb1d87942e888ca
kchilds2020 <63356648+kchilds2020@users.noreply.github.com>
2020-08-08 17:01:09 -0500

    added photo from type file

--- src/index.js
+++ src/index.js
@@ -1,6 +1,24 @@
-import React from 'react'
+import React, {useState} from 'react'
 import styles from './styles.module.css'
 
-export const ExampleComponent = ({ text }) => {
-  return <div className={styles.test}>Example Component: {text}</div>
+export const FileUpload = () => {
+
+  const [file, setFile] = useState('');
+
+  const handleChange = (e) => {
+    e.preventDefault();
+
+    let reader = new FileReader();
+    reader.onload = (e) => {
+      setFile(e.target.result)
+    };
+    reader.readAsDataURL(event.target.files[0]);
+  }
+
+  return (
+    <div>
+      <input type='file' onChange={handleChange}/>
+      {file !== '' ? <img src={file} alt='fileimg'/> : <div>no file</div> }
+    </div>
+    )
 }
